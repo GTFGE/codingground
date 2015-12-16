2755ee4553f96bfe6f3fe77e989fbe78b8fa814e
GTFGE <ecoley41874@jcss.us>
2015-12-16 13:12:50 -0500

    Created by Coding Ground Client

---
+++ New Project/HelloWorld.java
@@ -0,0 +1,11 @@
+public class BestCustomer
+{
+     public static void main(String []args)
+     {
+    Customer c1 = new Customer("Rose", 9.38);
+    Customer c2 = new Customer("Rick", 31.45);
+    Customer c3 = new Customer("Rory", 12.78);
+    System.out.println("Your three customers are: " + c1.getName() + " , " + c2.getName() + ", and " + c3.getName());
+    
+     }
+}
